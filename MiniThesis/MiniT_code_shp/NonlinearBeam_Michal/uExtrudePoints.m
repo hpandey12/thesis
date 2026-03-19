8aa9b8f9da3e63ac4743bf1a4a9d0461d715c6d7
warlike-penguin <harsh.pandey@rwth-aachen.de>
2026-03-19 15:17:35 +0100

    .

--- MiniThesis/MiniT_code_shp/NonlinearBeam_Michal/uExtrudePoints.m
+++
@@ -1,8 +0,0 @@
-function ExtrudedPoints = uExtrudePoints(TransformationMatrix,Points)
-% -------------------------------------------------------------------------
-%
-% -------------------------------------------------------------------------
-ExtrudedPoints = (TransformationMatrix * [Points ones(size(Points,1),1)]')';
-ExtrudedPoints = ExtrudedPoints(:,1:2);
-% -------------------------------------------------------------------------
-end
